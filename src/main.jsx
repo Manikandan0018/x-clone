a1aa34db53b8d356ab23f4004b0b1a030cb8edb6
manikandan <manikandan110305@gmail.com>
2025-08-11 02:22:45 +0530

    Fix package.json merge conflict

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
+
+// 1. Create a client
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. Provide the client */}
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter  basename="/x-clone">
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
